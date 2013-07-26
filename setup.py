5612a1838ce46a97bd0b92e6df1ec2fbb1751e2a
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2013-07-26 16:05:31 +0200

    Converting ckanext-swisstopo to a CKAN-style harvester

--- setup.py
+++ setup.py
@@ -6,15 +6,15 @@ version = '0.0'
 setup(
 	name='ckanext-swisstopo',
 	version=version,
-	description="swisstopo CKAN extension",
+	description="CKAN extension of the Federal Office of Topography swisstopo for the OGD portal of Switzerland",
 	long_description="""\
 	""",
 	classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
 	keywords='',
 	author='Liip AG',
-	author_email='contact@liip.ch',
+	author_email='ogd@liip.ch',
 	url='http://www.liip.ch',
-	license='',
+	license='GPL',
 	packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
 	namespace_packages=['ckanext', 'ckanext.swisstopo'],
 	include_package_data=True,
@@ -26,7 +26,9 @@ setup(
 	"""
     [ckan.plugins]
 	#swisstopo_plugin=ckanext.swisstopo:PluginClass
+    swisstopo=ckanext.swisstopo.plugins:SwisstopoHarvest
+    swisstopo_harvester=ckanext.swisstopo.harvester:SwisstopoHarvester
     [paste.paster_command]
-    swisstopo=ckanext.swisstopo.commands.swisstopo:SwisstopoCommand
+    swisstopo_harvest=ckanext.swisstopo.commands.harvester:Harvester
 	""",
 )
